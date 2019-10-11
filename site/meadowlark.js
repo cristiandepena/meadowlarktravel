8a9cb8bc2eb29897131f8f79c14ee8b4e89b6fb5
Cristian A. De Peña <dominicanxxx@gmail.com>
2019-10-10 23:33:09 -0400

    First commit

---
+++ site/meadowlark.js
@@ -0,0 +1,42 @@
+let express = require('express');
+let app = express();
+
+// Set up handlebars view engine
+let handlebars = require('express-handlebars')
+    .create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+
+// main
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+// Static Files middleware
+app.use(express.static(__dirname + '/public'));
+
+// 404 catch all handler
+app.use(function(req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+// 500 catch all handler
+app.use(function(req, res) {
+    res.type('text/plain');
+    req.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express started on htpp://localhost:' + app.get('port') + ';');
+
+})
